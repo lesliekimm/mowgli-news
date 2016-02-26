8fa6d6a581a0de480a593b8d980a5842041124db
leslie kimm <leslie.kimm@gmail.com>
2016-02-25 22:16:21 -0800

    add link to post form

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ angular.module('mowgliNews', [])
 
             $scope.addPost = function() {
                 if (!$scope.title || $scope.title === '') { return; }
-                $scope.posts.push({ title: 'A new post!', upvotes: 0 });
+                $scope.posts.push({
+                    title: $scope.title,
+                    link: $scope.link,
+                    upvotes: 0
+                });
                 $scope.title = '';
+                $scope.link = '';
             };
 
             $scope.incrementUpvotes = function(post) {
