bcec8fe73d37b60f812a7e0bd23f83b5fb2cbf5d
leslie kimm <leslie.kimm@gmail.com>
2016-02-28 14:30:48 -0800

    add factory

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 angular.module('mowgliNews', [])
-    .factory('posts', [function() {
-        var o = {
-            posts: []
+.factory('posts', [function(){
+    var o = {
+        posts: []
+    };
+    return o;
+}])
+
+.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function ($scope, posts) {
+        $scope.posts = posts.posts;
+
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') { return; }
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+            $scope.title = '';
+            $scope.link = '';
         };
-        return o;
-    }])
-    .controller('MainCtrl', [
-        '$scope',
-        'posts',
-        function($scope, posts) {
-            $scope.posts = posts.posts;
 
-            $scope.addPost = function() {
-                if (!$scope.title || $scope.title === '') { return; }
-                $scope.posts.push({
-                    title: $scope.title,
-                    link: $scope.link,
-                    upvotes: 0
-                });
-                $scope.title = '';
-                $scope.link = '';
-            };
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
 
-            $scope.incrementUpvotes = function(post) {
-                post.upvotes += 1;
-            };
-
-            $scope.decrementUpvotes = function(post) {
-                if (post.upvotes > 0) {
-                    post.upvotes -= 1;
-                }
-            };
-        }
-    ]
+        $scope.decrementUpvotes = function(post) {
+            if (post.upvotes > 0) {
+                post.upvotes -= 1;
+            }
+        };
+    }
+]
 );
