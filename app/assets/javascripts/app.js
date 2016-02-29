7439b4613b718b91abe12bbe48b980f34244f435
leslie kimm <leslie.kimm@gmail.com>
2016-02-28 22:33:38 -0800

    Add rails and set up root path

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -64,5 +64,25 @@ angular.module('mowgliNews', ['ui.router'])
     'posts',
     function($scope, $stateParams, posts) {
         $scope.post = posts.posts[$stateParams.id];
+
+        $scope.addComment = function() {
+            if ($scope.body === '') { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+
+        $scope.incrementUpvotes = function(comment) {
+            comment.upvotes += 1;
+        };
+
+        $scope.decrementUpvotes = function(comment) {
+            if (comment.upvotes > 0) {
+                comment.upvotes -= 1;
+            }
+        };
     }
 ]);
